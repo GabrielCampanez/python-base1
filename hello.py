780d261415dadaadc40850df460d426e906f49ce
Gabriel Campanez <gattiscampanez@gmail.com>
2023-01-24 09:54:26 -0300

    IMplementamos o hello word multi linguas

--- hello.py
+++ hello.py
@@ -1,7 +1,35 @@
 #!/usr/bin/env python3 
+"""Hello World Multi Linguas.
 
-# este programa imprime Hello World
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem
+correspondente.
 
-print("Hello, World!")
+Como usar:
+
+Tenha a variável LANG devidamente configurado ex:
+
+    export LANG=pt_BR
+
+Execução
+
+    python3 hello.py
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Gabriel Campanez"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+
+print(msg)
 
 
