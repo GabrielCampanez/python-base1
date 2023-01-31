195158880f48501efa2cc08258de6204de37f912
Gabriel Campanez <gattiscampanez@gmail.com>
2023-01-31 12:07:41 -0300

    Adicionado Rotating file handlers

--- logs.py
+++ logs.py
@@ -2,21 +2,29 @@
 
 import os
 import logging
+from logging import handlers
 
 # BOILERPLATE
 # TODO: usar função
 # TODO: usar lib (loguru)
 log_level = os.getenv("LOG_LEVEL", "WARNING").upper()
 log = logging.Logger("gabriel", log_level)
-ch = logging.StreamHandler()
-ch.setLevel(log_level)
+# ch = logging.StreamHandler()  #console/terminal/stderr
+# ch.setLevel(log_level)
+fh = handlers.RotatingFileHandler(
+    "meulog.log", 
+    maxBytes=400, # 10**6
+    backupCount=10,
+)
+fh.setLevel(log_level)
 fmt = logging.Formatter(
     '%(asctime)s %(name)s %(levelname)s'
     ' l:%(lineno)d f:%(filename)s: %(message)s'
 )
-ch.setFormatter(fmt)
-log.addHandler(ch)
-
+#ch.setFormatter(fmt)
+fh.setFormatter(fmt)
+#log.addHandler(ch)
+log.addHandler(fh)
 
 """
 log.debug("Mensagem pro dev, qe, sysadmin")
